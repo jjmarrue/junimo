848b600012f1f56725f0d2d122f005e32b5f97fa
Juan Mata <jjmarrue@gmail.com>
2022-08-30 17:12:47 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # junimo
+
+A short animation of a Junimo from the Stardew Valley video game. This is my first time playing around with SVG files and CSS animations.
---
+++ script.js
@@ -0,0 +1,8 @@
+// const svg = document.querySelector("#junimo");
+    
+// svg.onmouseover = (e) => {
+//     const colors = ['pink', 'yellow', 'gold', 'orange', 'lightGreen', 'olive', 'green', 'cyan', 'lightCyan', 'blue', 'purple', 'maroon', 'brown', 'wheat', 'lightGreen', 'grey', 'white'];
+//     const randColor = () => colors[Math.floor(Math.random()*colors.length)];
+//     document.documentElement.style.cssText = 
+//         `--junimo-color: ${randColor()};`
+//  }
